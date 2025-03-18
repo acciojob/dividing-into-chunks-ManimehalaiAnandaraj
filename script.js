702e50a761cffcad727604b44a687e2131949d2e
ManimehalaiAnandaraj <manimehalaiannakodi@gmail.com>
2025-03-18 10:18:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
+	let n = 2;
+
+
+let a1 = a.splice(0, n); 
+let a2 = a.splice(0, n); 
+let a3 = a.splice(0, n); 
+let a4 = a.splice(0, n);
+
+
+console.log( a1);
+console.log(a2);
+console.log(a3);
+console.log( a4);
 };
 
 const n = prompt("Enter n: ");
